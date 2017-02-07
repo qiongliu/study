2cb195527dbf07ebe38bac35b161aecdecfda571
qiongliu <273984995@qq.com>
2017-02-07 15:17:12 +0800

    reset

---
+++ es6/src/demo.js
@@ -0,0 +1,16 @@
+//定义类
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return '(' + this.x + ', ' + this.y + ')';
+  }
+}
+
+var p = new Point(1, 2);
+
+var str = p.toString();
+console.log(str);
