6b8c65f780c6d45a614dc8d78e82636c75c21dd8
林一 <273984995@qq.com>
2017-11-22 17:46:25 +0800

    insert

--- nodejs/note/mongo/app.js
+++ nodejs/note/mongo/app.js
@@ -3,15 +3,42 @@ const db = require('./models/db');
 
 let app = express();
 
-app.get('/',(req,res) => {
+app.get('/find',(req,res) => {
+	let page = parseInt(req.query.page) || 1,
+	conut = 5;
+	db.setUrl("mongodb://127.0.0.1/demo2");
+	db.find('info',{
+		'$or': [{'age':94},{'age': 4}]
+	},{
+		'page': page,
+		'count': conut
+	}).then((result) => {
+		res.send(result);
+	},(err) => {
+		res.end(err);
+	});
+});
+
+app.get('/insert',(req,res) => {
 	db.setUrl("mongodb://127.0.0.1/demo2");
 	db.insert('info',{
 		"name": 'b',
 		"age": parseInt(Math.random() * (100 - 1 + 1) + 1)
 	}).then((result) => {
 		res.end();
-	},(err) => {
-		console.log(err);
+	},(message) => {
+		res.end(message);
+	});
+});
+
+app.get('/delete',(req,res) => {
+	let age = parseInt(req.query.age);
+	db.delete('info',{
+		'age': age
+	}).then((result) => {
+		res.send(result);
+	},(message) => {
+		res.send(message);
 	});
 });
 
--- nodejs/note/mongo/models/db.js
+++ nodejs/note/mongo/models/db.js
@@ -6,9 +6,9 @@ function _connect () {
 	return new Promise((resolve,reject) => {
 		MongoClient.connect(dburl,(err,db) => {
 			if (err) {
-				console.log("数据库连接失败!");
-				return reject(err);
+				return reject("数据库连接失败!");
 			}
+			console.log(11);
 			resolve(db);
 		});
 	});
@@ -18,19 +18,53 @@ exports.setUrl = (url) => {
 	dburl = url || "mongodb://127.0.0.1/test";
 };
 
-exports.insert = (collection,data) => {
+exports.insert = (collectionName,data) => {
 	return new Promise((resolve,reject) => {
 		_connect().then((db) => {
-			db.collection(collection).insertOne(data,(err,result) => {
+			db.collection(collectionName).insertOne(data,(err,result) => {
 				db.close();
 				if (err) {
-					return reject(err);
+					return reject(err,"添加数据失败！");
 				}
 				resolve(result);
 			});
-		},(err) => {
-			reject(err);
+		},(message) => {
+			reject(message);
 		});
 	});
 };
+
+exports.find = (collectionName,data,opts) => {
+	return new Promise((resolve,reject) => {
+		opts = opts || {
+			'page': 0,
+			'count': 0
+		};
+		let limit = opts.count,
+		skip = (opts.page - 1) * opts.count;
+		_connect().then((db) => {
+			db.collection(collectionName).find(data).limit(limit).skip(skip).toArray((err,result) => {
+				if (err) return reject(err);
+				resolve(result);
+			})
+		},(message) => {
+			reject(message);
+		});
+	});
+};
+
+exports.delete = (collectionName,data) => {
+	return new Promise((resolve,reject) => {
+		_connect().then((db) => {
+			console.log(1);
+			db.collection(collectionName).deleteMany(data,(err,result) => {
+				if (err) return reject("删除失败！");
+				resolve(result);
+			});
+		},(message) => {
+			reject(message);
+		});
+	});
+};
+
 
